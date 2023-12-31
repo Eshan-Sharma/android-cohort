0b027c74b8c15ab79a4e5564db8e6e6c7eed5d39
Eshan-Sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2023-12-31 18:37:30 +0530

    week2 practice

---
+++ week2/Test1/app/src/main/java/com/example/test1/DriverClass.kt
@@ -0,0 +1,12 @@
+package com.example.test1
+
+class DriverClass {
+
+    internal object HelloWorld {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Hello, World!")
+        }
+    }
+
+}
